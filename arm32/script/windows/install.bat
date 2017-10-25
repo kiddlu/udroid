fe6341b9a1076af0c58089886c5dffd5ad638687
kiddlu <kidd.dawny.lu@gmail.com>
2017-10-25 10:48:39 +0000

    update
    Signed-off-by: kiddlu <kidd.dawny.lu@gmail.com>

--- arm32/script/windows/install.bat
+++ arm32/script/windows/install.bat
@@ -5,5 +5,5 @@ adb remount
 adb push ../../bin/busybox /system/bin/
 
 adb shell busybox mkdir -p /data/ubuntu
-adb push ..\..\dist\ubuntu-arm32.tar.gz /data/ubuntu
-adb shell busybox tar -zxvf /data/ubuntu/ubuntu-arm32.tar.gz -C /data/ubuntu
+adb push ..\..\dist\udroid.tar.gz /data/ubuntu
+adb shell busybox tar -zxvf /data/ubuntu/udroid.tar.gz -C /data/ubuntu
