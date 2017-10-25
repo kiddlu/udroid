48b1e9a008ed39924b1b31c88512639f0eab8b88
kiddlu <kidd.dawny.lu@gmail.com>
2017-10-25 10:53:46 +0000

    update
    Signed-off-by: kiddlu <kidd.dawny.lu@gmail.com>

--- arm32/script/windows/deploy.bat
+++ arm32/script/windows/deploy.bat
@@ -8,33 +8,33 @@ adb root
 
 adb push ../../sdcard /
 
-adb push ../../bin /data/ubuntu/usr/local
-adb shell busybox chmod -R a+x /data/ubuntu/usr/local/bin
+adb push ../../bin /data/udroid/usr/local
+adb shell busybox chmod -R a+x /data/udroid/usr/local/bin
 
-adb shell busybox umount /data/ubuntu/sdcard
-adb shell busybox umount /data/ubuntu/system
-adb shell busybox umount /data/ubuntu/sys/kernel/debug
-adb shell busybox umount /data/ubuntu/sys/fs/selinux
-adb shell busybox umount /data/ubuntu/sys
-adb shell busybox umount /data/ubuntu/dev/pts
-adb shell busybox umount /data/ubuntu/dev
-adb shell busybox umount /data/ubuntu/proc
+adb shell busybox umount /data/udroid/sdcard
+adb shell busybox umount /data/udroid/system
+adb shell busybox umount /data/udroid/sys/kernel/debug
+adb shell busybox umount /data/udroid/sys/fs/selinux
+adb shell busybox umount /data/udroid/sys
+adb shell busybox umount /data/udroid/dev/pts
+adb shell busybox umount /data/udroid/dev
+adb shell busybox umount /data/udroid/proc
 
-adb shell busybox mount -o bind /proc /data/ubuntu/proc
-adb shell busybox mount -o bind /dev /data/ubuntu/dev
-adb shell busybox mount -o bind /dev/pts /data/ubuntu/dev/pts
-adb shell busybox mount -o bind /sys /data/ubuntu/sys
-adb shell busybox mount -o bind /sys/fs/selinux /data/ubuntu/sys/fs/selinux
-adb shell busybox mount -o bind /sys/kernel/debug /data/ubuntu/sys/kernel/debug
-adb shell busybox mkdir -p /data/ubuntu/system
-adb shell busybox mkdir -p /data/ubuntu/sdcard
-adb shell busybox mount -o bind /system/ /data/ubuntu/system
-adb shell busybox mount -o bind /sdcard /data/ubuntu/sdcard
+adb shell busybox mount -o bind /proc /data/udroid/proc
+adb shell busybox mount -o bind /dev /data/udroid/dev
+adb shell busybox mount -o bind /dev/pts /data/udroid/dev/pts
+adb shell busybox mount -o bind /sys /data/udroid/sys
+adb shell busybox mount -o bind /sys/fs/selinux /data/udroid/sys/fs/selinux
+adb shell busybox mount -o bind /sys/kernel/debug /data/udroid/sys/kernel/debug
+adb shell busybox mkdir -p /data/udroid/system
+adb shell busybox mkdir -p /data/udroid/sdcard
+adb shell busybox mount -o bind /system/ /data/udroid/system
+adb shell busybox mount -o bind /sdcard /data/udroid/sdcard
 
-adb shell busybox cp -f property_contexts /data/ubuntu/
-adb shell busybox cp -f seapp_contexts /data/ubuntu/
-adb shell busybox cp -f selinux_version /data/ubuntu/
-adb shell busybox cp -f sepolicy /data/ubuntu/
-adb shell busybox cp -f service_contexts /data/ubuntu/
+adb shell busybox cp -f property_contexts /data/udroid/
+adb shell busybox cp -f seapp_contexts /data/udroid/
+adb shell busybox cp -f selinux_version /data/udroid/
+adb shell busybox cp -f sepolicy /data/udroid/
+adb shell busybox cp -f service_contexts /data/udroid/
 
-adb push ../../root  /data/ubuntu/
+adb push ../../root  /data/udroid/
--- arm32/script/windows/install.bat
+++ arm32/script/windows/install.bat
@@ -4,6 +4,6 @@ adb root
 adb remount
 adb push ../../bin/busybox /system/bin/
 
-adb shell busybox mkdir -p /data/ubuntu
-adb push ..\..\dist\udroid.tar.gz /data/ubuntu
-adb shell busybox tar -zxvf /data/ubuntu/udroid.tar.gz -C /data/ubuntu
+adb shell busybox mkdir -p /data/udroid
+adb push ..\..\dist\udroid.tar.gz /data/udroid
+adb shell busybox tar -zxvf /data/udroid/udroid.tar.gz -C /data/udroid
