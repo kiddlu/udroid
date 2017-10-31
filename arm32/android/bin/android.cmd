a3097b29be7ac1405caf17bea69dec6e724cd38b
kiddlu <kidd.dawny.lu@gmail.com>
2017-10-31 12:08:34 +0000

    update
    Signed-off-by: kiddlu <kidd.dawny.lu@gmail.com>

---
+++ arm32/android/bin/android.cmd
@@ -0,0 +1,30 @@
+Java Layer:
+am      
+appops   
+appwidget
+bmgr
+bu     
+content
+dpm        
+hid        
+ime       
+input      
+media      
+monkey
+pm 
+requestsync
+settings
+sm         
+svc     
+telecom 
+uiautomator
+wm
+
+Native Layer:
+dumpsys
+screencap
+vdc
+ndc
+debuggerd
+atrace
+....
--- arm32/android/bin/fbn.sh
+++ arm32/android/bin/fbn.sh
@@ -1,3 +1,5 @@
 #!/system/bin/sh
 
+PATH=/sbin:/vendor/bin:/system/sbin:/system/bin:/system/xbin
+
 toybox find . -name "*$1*"
