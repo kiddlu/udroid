06e29fae3b28a1b1b90466106d8f90ad063b0e18
kiddlu <kidd.dawny.lu@gmail.com>
2017-10-31 01:54:06 +0000

    update
    Signed-off-by: kiddlu <kidd.dawny.lu@gmail.com>

--- arm32/bin/setdns.sh
+++ arm32/bin/setdns.sh
@@ -2,8 +2,18 @@
 
 set -x
 
-nameserver=`getprop net.dns1`
-
 mkdir -p /run/resolvconf/
 
-echo nameserver $nameserver > /run/resolvconf/resolv.conf
+connectedVPN=`getprop sys.net.connectedVPN`
+
+if [[ -z "$connectedVPN" ]]; then
+  nameserver=`getprop net.dns1`
+  echo nameserver $nameserver > /run/resolvconf/resolv.conf
+  echo nameserver 223.5.5.5  >> /run/resolvconf/resolv.conf
+  echo nameserver 223.6.6.6  >> /run/resolvconf/resolv.conf
+else
+  nameserver=`getprop net.dns1`
+  echo nameserver $nameserver > /run/resolvconf/resolv.conf
+  echo nameserver 8.8.8.8    >> /run/resolvconf/resolv.conf
+  echo nameserver 8.8.4.4    >> /run/resolvconf/resolv.conf
+fi 
