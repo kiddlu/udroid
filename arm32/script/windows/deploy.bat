6c6875bdd50dc8d9d0fa76bc6854133e6c20d608
kiddlu <kidd.dawny.lu@gmail.com>
2017-11-03 07:02:09 +0000

    update
    Signed-off-by: kiddlu <kidd.dawny.lu@gmail.com>

--- arm32/script/windows/deploy.bat
+++ arm32/script/windows/deploy.bat
@@ -17,5 +17,6 @@ adb shell /system/bin/toybox cp -f seapp_contexts /data/ubuntu/
 adb shell /system/bin/toybox cp -f selinux_version /data/ubuntu/
 adb shell /system/bin/toybox cp -f sepolicy /data/ubuntu/
 adb shell /system/bin/toybox cp -f service_contexts /data/ubuntu/
+adb shell /system/bin/toybox cp -rf /data/ubuntu/etc /data/ubuntu/etc.bak
 adb shell /system/bin/toybox cp -Lrf /system/etc/* /data/ubuntu/etc/
 adb shell /system/bin/toybox cp -Lrf /sbin/* /data/ubuntu/sbin/
--- arm32/script/windows/install.bat
+++ arm32/script/windows/install.bat
@@ -2,8 +2,8 @@
 
 adb root
 adb remount
-adb push ../../bin/busybox /system/bin/
+adb push busybox /system/bin/
 
 adb shell busybox mkdir -p /data/ubuntu
-adb push ..\..\dist\ubuntu.tar.gz /data/ubuntu
+adb push  ubuntu.tar.gz /data/ubuntu
 adb shell busybox tar -zxvf /data/ubuntu/ubuntu.tar.gz -C /data/ubuntu
