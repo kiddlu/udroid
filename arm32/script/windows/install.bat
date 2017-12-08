c9b7e1f27213c15f7c716cc967229e282fc267e3
kiddlu <kidd.dawny.lu@gmail.com>
2017-12-08 02:50:26 +0000

    update
    Signed-off-by: kiddlu <kidd.dawny.lu@gmail.com>

--- arm32/script/windows/install.bat
+++ arm32/script/windows/install.bat
@@ -2,11 +2,14 @@
 
 adb root
 
+::set up term for android host adb
 adb install Term.apk
 adb push adb /data/user/0/jackpal.androidterm/app_HOME/
+adb shell /system/bin/toybox chmod a+x /data/user/0/jackpal.androidterm/app_HOME/adb
+adb shell setprop persist.adb.tcp.port 5555
 
+::unzip ubuntu rootfs
 adb push busybox /data/local/tmp/
-
 adb shell /system/bin/toybox chmod a+x /data/local/tmp/busybox
 adb shell /data/local/tmp/busybox mkdir -p /data/ubuntu
 adb push  ubuntu.tar.gz /data/ubuntu
