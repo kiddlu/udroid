8e6eac23e34699bbab7fbb3cd0d9ca306eaec6e7
kiddlu <kidd.dawny.lu@gmail.com>
2017-12-20 03:30:41 +0000

    update
    Signed-off-by: kiddlu <kidd.dawny.lu@gmail.com>

---
+++ arm32/android/shell/and-cmd.sh
@@ -0,0 +1,24 @@
+#!/system/bin/sh
+
+echo am
+echo app_process
+echo appops
+echo appwidget
+echo bmgr
+echo bu
+echo content
+echo dalvikvm
+echo dpm
+echo hid
+echo ime
+echo input
+echo media
+echo monkey
+echo pm
+echo requestsync
+echo settings
+echo sm
+echo svc
+echo telecom
+echo uiautomator
+echo wm
