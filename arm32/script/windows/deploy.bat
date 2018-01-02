a961d53e9faa85eb28e8878123d1dc199e2ccf87
kiddlu <kidd.dawny.lu@gmail.com>
2018-01-02 05:12:23 +0000

    update
    Signed-off-by: kiddlu <kidd.dawny.lu@gmail.com>

--- arm32/script/windows/deploy.bat
+++ arm32/script/windows/deploy.bat
@@ -3,7 +3,7 @@
 adb root
 
 adb push ../../sdcard     /
-adb push ../../android    /data/ubuntu/usr/local
+adb push ../../udroid    /data/ubuntu/usr/local
 adb push ../../root       /data/ubuntu/
 
 adb shell /system/bin/toybox chmod -R a+x /data/ubuntu/usr/local/android/bin
@@ -13,12 +13,3 @@ adb shell /system/bin/toybox chmod -R a+x /data/ubuntu/usr/local/android/wrapper
 
 adb shell /system/bin/toybox mkdir -p /data/ubuntu/system
 adb shell /system/bin/toybox mkdir -p /data/ubuntu/sdcard
-
-adb shell /system/bin/toybox cp -f property_contexts /data/ubuntu/
-adb shell /system/bin/toybox cp -f seapp_contexts /data/ubuntu/
-adb shell /system/bin/toybox cp -f selinux_version /data/ubuntu/
-adb shell /system/bin/toybox cp -f sepolicy /data/ubuntu/
-adb shell /system/bin/toybox cp -f service_contexts /data/ubuntu/
-adb shell /system/bin/toybox cp -rf /data/ubuntu/etc /data/ubuntu/etc.bak
-adb shell /system/bin/toybox cp -Lrf /system/etc/* /data/ubuntu/etc/
-adb shell /system/bin/toybox cp -Lrf /sbin/* /data/ubuntu/sbin/
---
+++ arm32/udroid/mksh/README.md
@@ -0,0 +1,2 @@
+This tools not use for udroid env, but for adb shell(mksh) only.
+u can build or get them on [termux](https://termux.com/).
