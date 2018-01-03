031a9f4fb3ee995d2310704fe8954b3a64cea8bb
kiddlu <kidd.dawny.lu@gmail.com>
2018-01-03 04:00:50 +0000

    udpate
    Signed-off-by: kiddlu <kidd.dawny.lu@gmail.com>

--- arm32/script/windows/deploy.bat
+++ arm32/script/windows/deploy.bat
@@ -6,8 +6,4 @@ adb push ../../sdcard     /
 adb push ../../udroid     /data/ubuntu/usr/local
 adb push ../../root       /data/ubuntu/
 
-adb shell /system/bin/toybox chmod -R a+x /data/ubuntu/usr/local/udroid/bin
-adb shell /system/bin/toybox chmod -R a+x /data/ubuntu/usr/local/udroid/shell
-adb shell /system/bin/toybox chmod -R a+x /data/ubuntu/usr/local/udroid/python
-adb shell /system/bin/toybox chmod -R a+x /data/ubuntu/usr/local/udroid/link
-adb shell /system/bin/toybox chmod -R a+x /data/ubuntu/usr/local/udroid/wrapper
+adb shell /system/bin/toybox chmod -R a+x /data/ubuntu/usr/local/udroid
